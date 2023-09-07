1c25999ba0102841f1ccd3e00adc2cfece53cdb6
L0STCOLE <93286109+L0STCOLE@users.noreply.github.com>
2023-09-06 19:55:05 -0500

    Add files via upload

---
+++ MyToken.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.8.5;
+// SPDX-License-Identifier: CPOL-1.02
+
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
+
+contract MyToken is ERC20 {
+    address public admin = 0x173bAb118A0Df0bB204441d0C6e8D3459c2EBE3C;
+    address public User;
+    mapping (address => uint) public balances;
+    
+    
+    constructor() ERC20('Cardell Token', 'Cardell Coin') {
+        _mint(admin, 10000 * 10 ** 18);
+        
+    }
+
+      function GetUser(address hello) public{
+            User = hello;
+        }
+      function mint( uint amount) external {
+         require(msg.sender == admin, 'only admin');
+         _mint(msg.sender, amount);
+    }
+
+    function burn(uint amount) external {
+        _burn(msg.sender, amount);
+    }
+}
+contract ERC20WithMinerReward is ERC20, MyToken {
+
+     function mintMinterReward() public { 
+            _mint(User, 1000);
+
+        } 
+}       
